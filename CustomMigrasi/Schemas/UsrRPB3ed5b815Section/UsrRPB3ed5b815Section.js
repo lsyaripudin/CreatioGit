a2c9b2821bb387749293d1e93450fe6db93ad619
Lili Syaripudin <lsyaripudin@gmail.com>
2025-10-15 14:15:49 +0700

    Push Custom Migrasi

---
+++ CustomMigrasi/Schemas/UsrRPB3ed5b815Section/UsrRPB3ed5b815Section.js
@@ -0,0 +1,8 @@
+define("UsrRPB3ed5b815Section", [], function() {
+	return {
+		entitySchemaName: "UsrRPB",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
